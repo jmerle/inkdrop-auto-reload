18d9f76a4dc819e1638f2a0ad67ce12625f3b0fd
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-29 00:52:41 +0100

    Postpone reload until focus and save before reload

--- dist/index.js
+++ dist/index.js
@@ -5,24 +5,55 @@ Object.defineProperty(exports, '__esModule', {
 });
 exports.activate = activate;
 exports.deactivate = deactivate;
+exports.config = void 0;
 
 var _chokidar = _interopRequireDefault(require('chokidar'));
-
-var _lodash = _interopRequireDefault(require('lodash.debounce'));
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
 }
 
 let watcher = null;
+let reloadCalled = false;
+
+function doReload() {
+  inkdrop.commands.add(document.body, {
+    'core:save-note': () => inkdrop.window.reload(),
+  });
+  inkdrop.commands.dispatch(document.body, 'core:save-note');
+}
+
+function reload() {
+  if (reloadCalled) {
+    return;
+  }
+
+  reloadCalled = true;
+  const reloadImmediately = inkdrop.config.get('auto-reload.reloadImmediately');
+  const focused = inkdrop.window.isFocused();
+
+  if (reloadImmediately || focused) {
+    doReload();
+  } else {
+    inkdrop.window.once('focus', () => {
+      doReload();
+    });
+  }
+}
+
+const config = {
+  reloadImmediately: {
+    title: 'Reload immediately when a change is detected',
+    type: 'boolean',
+    default: false,
+  },
+};
+exports.config = config;
 
 function activate() {
   watcher = _chokidar.default.watch(inkdrop.packages.getPackageDirPaths(), {
     ignoreInitial: true,
   });
-  const reload = (0, _lodash.default)(() => {
-    inkdrop.window.reload();
-  }, 250);
   watcher
     .on('add', () => reload())
     .on('change', () => reload())
@@ -32,6 +63,7 @@ function activate() {
 function deactivate() {
   if (watcher !== null) {
     watcher.close();
+    watcher = null;
   }
 }
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import chokidar from 'chokidar';
-import debounce from 'lodash.debounce';
 
 let watcher = null;
+let reloadCalled = false;
+
+function doReload() {
+  inkdrop.commands.add(document.body, {
+    'core:save-note': () => inkdrop.window.reload(),
+  });
+
+  inkdrop.commands.dispatch(document.body, 'core:save-note');
+}
+
+function reload() {
+  if (reloadCalled) {
+    return;
+  }
+
+  reloadCalled = true;
+
+  const reloadImmediately = inkdrop.config.get('auto-reload.reloadImmediately');
+  const focused = inkdrop.window.isFocused();
+
+  if (reloadImmediately || focused) {
+    doReload();
+  } else {
+    inkdrop.window.once('focus', () => {
+      doReload();
+    });
+  }
+}
+
+export const config = {
+  reloadImmediately: {
+    title: 'Reload immediately when a change is detected',
+    type: 'boolean',
+    default: false,
+  },
+};
 
 export function activate() {
   watcher = chokidar.watch(inkdrop.packages.getPackageDirPaths(), {
     ignoreInitial: true,
   });
-
-  const reload = debounce(() => {
-    inkdrop.window.reload();
-  }, 250);
 
   watcher
     .on('add', () => reload())
@@ -21,5 +52,6 @@ export function activate() {
 export function deactivate() {
   if (watcher !== null) {
     watcher.close();
+    watcher = null;
   }
 }
