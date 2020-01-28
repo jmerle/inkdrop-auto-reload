07a0e9481c6b66884b6678041f38b3c6d3451656
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-28 23:11:12 +0100

    Initial implementation

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,37 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 exports.activate = activate;
 exports.deactivate = deactivate;
 
+var _chokidar = _interopRequireDefault(require('chokidar'));
+
+var _lodash = _interopRequireDefault(require('lodash.debounce'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+let watcher = null;
+
 function activate() {
-  console.log('Plugin activated');
+  watcher = _chokidar.default.watch(inkdrop.packages.getPackageDirPaths(), {
+    ignoreInitial: true,
+  });
+  const reload = (0, _lodash.default)(() => {
+    inkdrop.window.reload();
+  }, 250);
+  watcher
+    .on('add', () => reload())
+    .on('change', () => reload())
+    .on('unlink', () => reload());
 }
 
 function deactivate() {
-  console.log('Plugin deactivated');
+  if (watcher !== null) {
+    watcher.close();
+  }
 }
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+import chokidar from 'chokidar';
+import debounce from 'lodash.debounce';
+
+let watcher = null;
+
 export function activate() {
-  console.log('Plugin activated');
+  watcher = chokidar.watch(inkdrop.packages.getPackageDirPaths(), {
+    ignoreInitial: true,
+  });
+
+  const reload = debounce(() => {
+    inkdrop.window.reload();
+  }, 250);
+
+  watcher
+    .on('add', () => reload())
+    .on('change', () => reload())
+    .on('unlink', () => reload());
 }
 
 export function deactivate() {
-  console.log('Plugin deactivated');
+  if (watcher !== null) {
+    watcher.close();
+  }
 }
