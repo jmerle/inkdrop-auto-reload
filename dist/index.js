55fee11c18aa25de0347dbd28e93e56913504f7b
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-29 01:59:21 +0100

    Update dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,22 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
 exports.activate = activate;
 exports.deactivate = deactivate;
 exports.config = void 0;
 
-var _chokidar = _interopRequireDefault(require('chokidar'));
+var _chokidar = _interopRequireDefault(require("chokidar"));
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 let watcher = null;
 let reloadQueued = false;
 
 function doReload() {
   inkdrop.commands.add(document.body, {
-    'core:save-note': () => inkdrop.window.reload(),
+    'core:save-note': () => inkdrop.window.reload()
   });
   inkdrop.commands.dispatch(document.body, 'core:save-note');
 }
@@ -52,28 +50,24 @@ function reload(path) {
 const config = {
   reloadImmediately: {
     title: 'Reload immediately when a change has been detected',
-    description:
-      'When disabled reloading is postponed until Inkdrop is given focus. Reloading causes Inkdrop to steal focus from other applications, which can be quite annoying.',
+    description: 'When disabled reloading is postponed until Inkdrop is given focus. Reloading causes Inkdrop to steal focus from other applications, which can be quite annoying.',
     type: 'boolean',
-    default: false,
+    default: false
   },
   reloadDisabled: {
     title: 'Reload when a change in a disabled plugin has been detected',
     type: 'boolean',
-    default: false,
-  },
+    default: false
+  }
 };
 exports.config = config;
 
 function activate() {
   watcher = _chokidar.default.watch(inkdrop.packages.getPackageDirPaths(), {
     ignoreInitial: true,
-    ignored: path => path.includes('.git/') || path.includes('node_modules/'),
+    ignored: path => path.includes('.git/') || path.includes('node_modules/')
   });
-  watcher
-    .on('add', path => reload(path))
-    .on('change', path => reload(path))
-    .on('unlink', path => reload(path));
+  watcher.on('add', path => reload(path)).on('change', path => reload(path)).on('unlink', path => reload(path));
 }
 
 function deactivate() {
